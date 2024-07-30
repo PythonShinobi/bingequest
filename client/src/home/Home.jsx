5158c41b2628958eccf2184921dd7abddefac50f
PythonShinobi <pythonshinobi@gmail.com>
2024-07-30 09:46:44 +0300

    Set up a home page template.

---
+++ client/src/home/Home.jsx
@@ -0,0 +1,13 @@
+// client/src/home/Home.jsx
+import React from "react";
+
+import "./Home.css";
+import Navbar from "../navbar/Navbar";
+
+const Home = () => {
+  return (
+    <Navbar />
+  );
+};
+
+export default Home;
