21f3becf55be3d5e78adb567b27969b41ad716b1
PythonShinobi <pythonshinobi@gmail.com>
2024-07-30 17:23:45 +0300

    register the now playing route path.

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./home/Home"
 import Register from './register/Register';
 import Login from './login/Login';
+import NowPlaying from './now_playing/NowPlaying';
 
 const MemoizedHome = memo(Home);
 const MemoizedRegister = memo(Register);
 const MemoizedLogin = memo(Login);
+const MemoizedNowPlaying = memo(NowPlaying)
 
 const App = () => {
   return (
@@ -17,6 +19,7 @@ const App = () => {
         <Route path='/' element={<MemoizedHome />} />
         <Route path='/Register' element={<MemoizedRegister />} />
         <Route path='/login' element={<MemoizedLogin />} />
+        <Route path='/now-playing' element={<MemoizedNowPlaying />} />
       </Routes>
     </BrowserRouter>
   );
