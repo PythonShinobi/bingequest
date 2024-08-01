f8d663a0c49e6271d2be16af8d55465a4b5f2099
PythonShinobi <pythonshinobi@gmail.com>
2024-08-01 11:14:02 +0300

    Add endpoint for upcoming movies.

--- server/app/movie/routes.py
+++ server/app/movie/routes.py
@@ -81,3 +81,28 @@ def get_top_rated_movies():
         return jsonify(response.json())
     else:
         return jsonify({"error": "Unable to fetch data from TMDb"}), response.status_code
+
+@bp.route('/movies/upcoming', methods=['GET'])
+def get_upcoming_movies():
+    token = Config.ACCESS_TOKEN
+    page = request.args.get('page', 1)
+
+    url = "https://api.themoviedb.org/3/discover/movie"
+    
+    params = {
+        "language": request.args.get('language', 'en-US'),
+        "sort_by": request.args.get('sort_by', 'release_date.desc'),
+        "page": page
+    }
+    
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+    
+    response = requests.get(url, headers=headers, params=params)
+    
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Unable to fetch data from TMDb"}), response.status_code    
