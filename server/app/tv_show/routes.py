92f6d6ea4315857442de1c4f8eef12156a017b22
PythonShinobi <pythonshinobi@gmail.com>
2024-08-02 15:09:45 +0300

    Add endpoint to fetch trending tv shows from the api.

--- server/app/tv_show/routes.py
+++ server/app/tv_show/routes.py
@@ -107,3 +107,26 @@ def get_top_rated_movies():
     else:
         print("Error Response:", response.json())
         return jsonify({"error": "Unable to fetch data from TMDb"}), response.status_code
+
+@bp.route('/tv-shows/trending', methods=['GET'])
+def get_trending_shows():
+    token = Config.ACCESS_TOKEN
+    
+    params = {
+        "language": request.args.get('language', 'en-US'),
+        "page": request.args.get('page', 1),
+    }
+    
+    url = "https://api.themoviedb.org/3/trending/tv/day"
+    
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+    
+    response = requests.get(url, headers=headers, params=params)
+    
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Unable to fetch data from TMDb"}), response.status_code
