343ddf5bce24eca36976f2f071669a4d96678747
PythonShinobi <pythonshinobi@gmail.com>
2024-08-03 09:24:45 +0300

    Add endpoint to search for actors by their name from the API.

--- server/app/people/routes.py
+++ server/app/people/routes.py
@@ -26,3 +26,28 @@ def get_popular_people():
         return jsonify(response.json())
     else:
         return jsonify({"error": "Unable to fetch data from TMDb"}), response.status_code
+    
+@bp.route('/search/popular', methods=['GET'])        
+def search_popular_person():
+    token = Config.ACCESS_TOKEN
+
+    params = {
+        "query": request.args.get("query", ""),
+        "language": request.args.get('language', 'en-US'),
+        "page": request.args.get('page', 1),
+        "include_adult": request.args.get('include_adult', 'false'),
+    }
+
+    url = "https://api.themoviedb.org/3/search/person"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Unable to fetch data from TMDb"}), response.status_code
