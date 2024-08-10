5b796223190c3951eddf0da6ebfa07b03b15527a
PythonShinobi <pythonshinobi@gmail.com>
2024-08-10 13:47:02 +0300

    Add about page.

---
+++ client/src/about/About.jsx
@@ -0,0 +1,15 @@
+// client/src/about/About.jsx
+import React from "react";
+
+import Navbar from "../navbar/Navbar";
+
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <p>This is an about page</p>
+    </div>
+  );
+};
+
+export default About;
