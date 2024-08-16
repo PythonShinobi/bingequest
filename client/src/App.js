c88e68ae8e22fac90ad8537571ef4904a4fccef0
PythonShinobi <pythonshinobi@gmail.com>
2024-08-16 15:21:43 +0300

    Refactor memo components and put them in a json format.

--- client/src/App.js
+++ client/src/App.js
@@ -25,55 +25,62 @@ import About from './about/About';
 import CelebrityDetails from './details/PersonDetails';
 
 import useIsAuthenticated from './redux/authHook';
+import Footer from './footer/Footer';
 
-const MemoizedHome = memo(Home);
-const MemoizedRegister = memo(Register);
-const MemoizedLogin = memo(Login);
-const MemoizedPopularMovies = memo(PopularMovies);
-const MemoizedTrendingMovies = memo(TrendingMovies);
-const MemoizedTopRatedMovies = memo(TopRatedMovies);
-const MemoizedUpcomingMovies = memo(UpcomingMovies);
-const MemoizedPopularTVShows = memo(PopularTVShows);
-const MemoizedAiringTVShows = memo(AiringTVShows);
-const MemoizedTopRatedShows = memo(TopRatedShows);
-const MemoizedTrendingTVShows = memo(TrendingTVShows);
-const MemoizedPopularPeople = memo(PopularPeople);
-const MemoizedSearchMovie = memo(SearchMovie);
-const MemoizedSearchTVShow = memo(SearchTVShow);
-const MemoizedMovieDetails = memo(MovieDetails);
-const MemoizedTvShowDetails = memo(TvShowDetails);
-const MemoizedProfilePage = memo(ProfilePage);
-const MemoizedPageNotFound = memo(PageNotFound);
-const MemoizedContactPage = memo(ContactPage);
-const MemoizedAbout = memo(About);
-const MemoizedCelebrityDetails = memo(CelebrityDetails);
+// Memoized components
+const MemoizedComponents = {
+  Home: memo(Home),
+  Register: memo(Register),
+  Login: memo(Login),
+  PopularMovies: memo(PopularMovies),
+  TrendingMovies: memo(TrendingMovies),
+  TopRatedMovies: memo(TopRatedMovies),
+  UpcomingMovies: memo(UpcomingMovies),
+  PopularTVShows: memo(PopularTVShows),
+  AiringTVShows: memo(AiringTVShows),
+  TopRatedShows: memo(TopRatedShows),
+  TrendingTVShows: memo(TrendingTVShows),
+  PopularPeople: memo(PopularPeople),
+  SearchMovie: memo(SearchMovie),
+  SearchTVShow: memo(SearchTVShow),
+  MovieDetails: memo(MovieDetails),
+  TvShowDetails: memo(TvShowDetails),
+  ProfilePage: memo(ProfilePage),
+  PageNotFound: memo(PageNotFound),
+  ContactPage: memo(ContactPage),
+  About: memo(About),
+  CelebrityDetails: memo(CelebrityDetails),
+};
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<MemoizedHome />} />
-        <Route path='/Register' element={<MemoizedRegister />} />
-        <Route path='/login' element={<MemoizedLogin />} />
-        <Route path='/contact' element={<MemoizedContactPage />} />
-        <Route path='/about' element={<MemoizedAbout />} />
-        <Route path='/movies/popular' element={<MemoizedPopularMovies />} />
-        <Route path='/movies/trending' element={<MemoizedTrendingMovies />} />
-        <Route path='/movies/top-rated' element={<MemoizedTopRatedMovies />} />
-        <Route path='/movies/upcoming' element={<MemoizedUpcomingMovies />} />
-        <Route path='/tv-shows/popular' element={<MemoizedPopularTVShows />} />
-        <Route path='/tv-shows/airing-today' element={<MemoizedAiringTVShows />} />
-        <Route path='/tv-shows/top-rated' element={<MemoizedTopRatedShows />} />
-        <Route path='/tv-shows/trending' element={<MemoizedTrendingTVShows />} />
-        <Route path='/people/popular' element={<MemoizedPopularPeople />} />
-        <Route path='/movies/search' element={<MemoizedSearchMovie />} />
-        <Route path='/tv-shows/search' element={<MemoizedSearchTVShow />} />
-        <Route path="/movie/:movieId" element={<MemoizedMovieDetails />} />
-        <Route path="/tv-show/:showId" element={<MemoizedTvShowDetails />} />
-        <Route path="/people/:personId" element={<MemoizedCelebrityDetails />} />
-        <Route path="/profile" element={<PrivateRoute component={MemoizedProfilePage} />} />
-        <Route path='*' element={<MemoizedPageNotFound />} />
-      </Routes>
+      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Routes>
+          <Route path='/' element={<MemoizedComponents.Home />} />
+          <Route path='/register' element={<MemoizedComponents.Register />} />
+          <Route path='/login' element={<MemoizedComponents.Login />} />
+          <Route path='/contact' element={<MemoizedComponents.ContactPage />} />
+          <Route path='/about' element={<MemoizedComponents.About />} />
+          <Route path='/movies/popular' element={<MemoizedComponents.PopularMovies />} />
+          <Route path='/movies/trending' element={<MemoizedComponents.TrendingMovies />} />
+          <Route path='/movies/top-rated' element={<MemoizedComponents.TopRatedMovies />} />
+          <Route path='/movies/upcoming' element={<MemoizedComponents.UpcomingMovies />} />
+          <Route path='/tv-shows/popular' element={<MemoizedComponents.PopularTVShows />} />
+          <Route path='/tv-shows/airing-today' element={<MemoizedComponents.AiringTVShows />} />
+          <Route path='/tv-shows/top-rated' element={<MemoizedComponents.TopRatedShows />} />
+          <Route path='/tv-shows/trending' element={<MemoizedComponents.TrendingTVShows />} />
+          <Route path='/people/popular' element={<MemoizedComponents.PopularPeople />} />
+          <Route path='/movies/search' element={<MemoizedComponents.SearchMovie />} />
+          <Route path='/tv-shows/search' element={<MemoizedComponents.SearchTVShow />} />
+          <Route path="/movie/:movieId" element={<MemoizedComponents.MovieDetails />} />
+          <Route path="/tv-show/:showId" element={<MemoizedComponents.TvShowDetails />} />
+          <Route path="/people/:personId" element={<MemoizedComponents.CelebrityDetails />} />
+          <Route path="/profile" element={<PrivateRoute component={MemoizedComponents.ProfilePage} />} />
+          <Route path='*' element={<MemoizedComponents.PageNotFound />} />
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 };
