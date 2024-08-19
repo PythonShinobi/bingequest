d534c7f90fa0ed7e9aecdf5b397589af0d8ec8ed
PythonShinobi <pythonshinobi@gmail.com>
2024-08-19 10:53:49 +0300

    Update run.py and config for vercel.

--- server/run.py
+++ server/run.py
@@ -3,13 +3,8 @@ import sqlalchemy
 from app import create_app, db
 from app.models import User
 
-flask_app = create_app()
+app = create_app()
 
-@flask_app.shell_context_processor
+@app.shell_context_processor
 def make_shell_context():
     return {'sqlalchemy': sqlalchemy, 'db': db, 'User': User}
-
-# Check if the script is being run directly
-if __name__ == '__main__':
-    # Run the Flask application on port 5000
-    flask_app.run(debug=False, port=5000)
