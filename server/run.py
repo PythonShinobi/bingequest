3840bafab46558b36f65ccdcf89f489ad1888a51
PythonShinobi <pythonshinobi@gmail.com>
2024-08-20 07:54:04 +0300

    Apply migration code in run.py

--- server/run.py
+++ server/run.py
@@ -1,4 +1,5 @@
 import sqlalchemy
+from flask_migrate import upgrade
 
 from app import create_app, db
 from app.models import User
@@ -8,3 +9,11 @@ app = create_app()
 @app.shell_context_processor
 def make_shell_context():
     return {'sqlalchemy': sqlalchemy, 'db': db, 'User': User}
+
+@app.before_request
+def apply_migrations():
+    with app.app_context():
+        upgrade()  # Apply migrations automatically
+
+if __name__ == "__main__":
+    app.run()
